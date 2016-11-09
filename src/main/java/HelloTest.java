75013ec159bc88746da227c15e264f677a1d0035
jupiter90 <diwenxu@seekersadvisors.com>
2016-11-09 10:47:39 +0800

    Initial commit

---
+++ src/main/java/HelloTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jupiter on 11/9/16.
+ */
+public class HelloTest {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Diwen");
+    }
+}
