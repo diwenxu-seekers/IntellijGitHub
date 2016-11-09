622d723b88d6bd4e4d28e6a87b5faf537c399c39
jupiter90 <diwenxu@seekersadvisors.com>
2016-11-09 15:22:44 +0800

    add new java

---
+++ src/main/java/hello/TestMain.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Created by jupiter on 11/9/16.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        System.out.print("Test Pipeline");
+
+
+    }
+}
